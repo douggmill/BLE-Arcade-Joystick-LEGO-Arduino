059c0f1510952e73603ac88d788dff2eaeff0b71
douggmill <douglas.gammill@gmail.com>
2023-04-22 00:34:50 -0500

    Added an assembly instruction pdf

--- ARCADEv3/ARCADEv3.ino
+++ ARCADEv3/ARCADEv3.ino
@@ -7,13 +7,10 @@
  */
 
 #define BOUNCE_WITH_PROMPT_DETECTION  // Make button state changes available immediately
-
 #include <Arduino.h>
 #include <Bounce2.h>     // https://github.com/thomasfredericks/Bounce2
 #include <BleGamepad.h>  // https://github.com/lemmingDev/ESP32-BLE-Gamepad
-
 #define numOfButtons 13
-
 Bounce debouncers[numOfButtons];
 
 //BleGamepad bleGamepad("Name1", "Name1", 100);
@@ -21,7 +18,6 @@ BleGamepad bleGamepad("Name2", "Name2", 100);
 
 byte buttonPins[numOfButtons] = { 4, 14, 16, 17, 19, 21, 22, 23, 25, 26, 27, 32, 33 };
 byte physicalButtons[numOfButtons] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-
 void setup() {
   for (byte currentPinIndex = 0; currentPinIndex < numOfButtons; currentPinIndex++) {
     pinMode(buttonPins[currentPinIndex], INPUT_PULLUP);
@@ -29,19 +25,16 @@ void setup() {
     debouncers[currentPinIndex].attach(buttonPins[currentPinIndex]);  // After setting up the button, setup the Bounce instance :
     debouncers[currentPinIndex].interval(5);
   }
-
   BleGamepadConfiguration bleGamepadConfig;
   bleGamepadConfig.setButtonCount(numOfButtons);
   bleGamepadConfig.setAutoReport(false);
   bleGamepad.begin(&bleGamepadConfig);
 }
-
 void loop() {
   if (bleGamepad.isConnected()) {
     bool sendReport = false;
     for (byte currentIndex = 0; currentIndex < numOfButtons; currentIndex++) {
       debouncers[currentIndex].update();
-
       if (debouncers[currentIndex].fell()) {
         bleGamepad.press(physicalButtons[currentIndex]);
         sendReport = true;
